78b657dfb45813e9572114984a8aae6ace3f2379
Albertoseiji <alberto.assatoalberto@gmail.com>
2023-08-15 06:15:23 -0300

    tarefa salario

---
+++ Salario/index.js
@@ -0,0 +1,25 @@
+// criando o array de salários
+const salarios = [1500, 2200, 1800, 2500, 2100, 1900, 2800, 1700, 2300, 1950];
+
+// usando o método map() para aplicar os aumentos de salário
+const salariosAumentados = salarios.map(salario => {
+  if (salario <= 2000) {
+    // aumento de 15%
+    return salario * 1.15;
+  } else {
+    // aumento de 10%
+    return salario * 1.10;
+  }
+});
+
+// usando o método filter() para filtrar os salários acima de 2500
+const salariosSuperiores2500 = salariosAumentados.filter(salario => salario > 2500);
+
+// usando o método reduce() para somar os salários
+const totalSalarios = salariosAumentados.reduce((acumulador, salario) => acumulador + salario, 0);
+
+// mostrar os valores
+console.log("Salários originais:", salarios);
+console.log("Salários com aumentos:", salariosAumentados);
+console.log("Salários acima de 2500:", salariosSuperiores2500);
+console.log("Total de salários:", totalSalarios);
